182aadc96cda94b866b1eceb8fab54bb037c52e8
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-02-04 18:18:29 +0530

    Create dff_no_reset.v

--- self-learn/artifact-05-dff/dff_no_reset.v
+++ self-learn/artifact-05-dff/dff_no_reset.v
@@ -10,17 +10,3 @@ module dff(
     Q<=D; //non-blocking assignments for memory elements
   end
 endmodule
-
-//with Reset
-module dff(
-  input D,Rst,
-  output Q
-);
-
-  always @(posedge clk, Rst) begin
-    if (Rst)
-      Q<=0;
-    else
-      Q<=D;
-  end
-endmodule

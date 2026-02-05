dfe6ca9c5863fbe856358f88afd4f0bbcb5eb8f1
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-02-05 09:49:17 +0530

    Create dff_async_reset.v

---
+++ self-learn/artifact-05-dff/dff_async_reset.v
@@ -0,0 +1,13 @@
+//D-flipflop with Asynchronous reset
+module dff_async_reset(
+  input clk,reset,D,
+  output reg Q
+);
+
+  always @(posedge clk or posedge reset) begin
+    if(reset)
+      Q<=0;
+    else
+      Q<=D;
+  end
+endmodule

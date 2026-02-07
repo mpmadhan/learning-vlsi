65e3d6ea41daea65c3a7f122bce66a467a519f3e
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-02-07 15:19:52 +0530

    Update dff_sync_reset_tb.v

--- self-learn/artifact-05-dff/dff_sync_reset_tb.v
+++ self-learn/artifact-05-dff/dff_sync_reset_tb.v
@@ -1,18 +1,22 @@
 //Testbench for D-flipflop with synchronous reset
+`timescale 1ns/1ps
+
 module dff_sync_reset_tb();
+  //1.Signal declaration
   reg clk;
   reg D;
   reg reset;
   wire Q;
-
+  //2.DUT instantiation
   dff_sync_reset dut(.clk(clk),.D(D),.reset(reset),.Q(Q));
-
-  initial begin    //creating clk
+  //3.Clock generation
+  initial begin
     clk=0;
     forever #5 clk=~clk;
   end
-
+  //4.Stimulus
   initial begin
+    $display("Time | clk reset D | Q");
     reset=1;D=0; //applying reset
     #13 reset=0; //removing reset next clock edge, it will apply
     #10 D=1; //changing input and checking q in next clock edge
@@ -20,8 +24,10 @@ module dff_sync_reset_tb();
     #10 reset=1; //applying reset
     #10 D=1; //checking whether input changes when reset is ON
     #10 reset=0; //removing reset
+    #20 $finish;
   end
-
+  //5.Observation
   always @(posedge clk) begin
-    $display ("%b %b %b | %b",clk,reset,D,Q);
+    $display ("%4t | %b %b %b | %b",$time,clk,reset,D,Q);
   end
+endmodule

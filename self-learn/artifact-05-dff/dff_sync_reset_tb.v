3584d1f1756e76979b2ebb9c2a8574f9bf5ec7ba
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-02-07 15:02:53 +0530

    Update dff_sync_reset_tb.v

--- self-learn/artifact-05-dff/dff_sync_reset_tb.v
+++ self-learn/artifact-05-dff/dff_sync_reset_tb.v
@@ -13,10 +13,13 @@ module dff_sync_reset_tb();
   end
 
   initial begin
-    reset=1;D=0;
-    #13 reset=0;
-    #10 D=0;
-    #10 D=1;
+    reset=1;D=0; //applying reset
+    #13 reset=0; //removing reset next clock edge, it will apply
+    #10 D=1; //changing input and checking q in next clock edge
+    #10 D=0; 
+    #10 reset=1; //applying reset
+    #10 D=1; //checking whether input changes when reset is ON
+    #10 reset=0; //removing reset
   end
 
   always @(posedge clk) begin

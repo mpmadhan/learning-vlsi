9b7f51bf496ed83c7b20cd2384692ac30a409493
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-02-08 16:45:39 +0530

    Create up_counter.v

---
+++ self-learn/artifact-07-up-counter/up_counter.v
@@ -0,0 +1,16 @@
+//4-bit Up-counter
+module up_counter(
+  input clk,
+  input reset,
+  input enable,
+  output reg [3:0] count
+);
+  always @(posedge clk) begin
+    if(reset)               //if reset=1 => count=0
+      count <= 0;
+    else if(enable)         //else if enable is 1 => count increments
+      count <= count + 1'b1;
+    //if reset=0, enable = 0 => count holds
+  end
+endmodule
+      

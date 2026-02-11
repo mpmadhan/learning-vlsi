ecf71334892e99e703bfb997cdcd417b77570115
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-02-11 23:24:54 +0530

    Update enable_clock.v

--- self-learn/artifact-09-enable-pulse/enable_clock.v
+++ self-learn/artifact-09-enable-pulse/enable_clock.v
@@ -1,17 +1,28 @@
-//RTL Code for Producing pulse for Enable signal
-module enable_clock #(parameter PULSE_PERIOD = 5)(
+/*RTL Code for producing Enable signal
+Basic Theory: Creating enable with the help of count,
+Count is a counter that counts the number of posedges,
+Once the amount of posedges(mentioned in PULSE_WIDTH) is completed enable signal is 1 for 1 clock cycle. */
+
+module enable_clock #(parameter PULSE_WIDTH = 5)(
   input clk,
   input reset,
   output reg en_pulse
 );
-  reg [PULSE_PERIOD-1:0] count;
+//$clog2(N) gives the minimum number of bits required to represent (N-1).
+  reg [$clog2(PULSE_WIDTH)-1:0] count;
   always @(posedge clk) begin
-    if(reset)
-      en_pulse<=0;
-    else
-      count<=count+1;
+    if(reset) begin
+      en_pulse <= 0;
+      count <= 0;
+    end
+    else begin
+      if(count==(PULSE_WIDTH-1)) begin
+        count <= 0;
+        en_pulse <= 1'b1;
+      end
+      else begin
+        count <= count + 1'b1;
+        en_pulse <= 1'b0;
+      end
+    end
   end
-  initial begin
-    e
-      
-  

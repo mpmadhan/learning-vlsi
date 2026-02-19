f9c9cb519f721562367f9014072cbf48c657d01b
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-02-19 12:54:14 +0530

    Update traffic_signal_fsm.v

--- self-learn/artifact-10-fsm/traffic_signal_fsm.v
+++ self-learn/artifact-10-fsm/traffic_signal_fsm.v
@@ -1,39 +1,36 @@
 /*RTL Code for FSM demonstration of traffic signal
 Red = 5 cycles, Green = 5 cycles, Yellow = 2 cycles */
 module traffic_signal_fsm(
+  //1. Ports
   input clk,
   input reset,
   output reg red,
   output reg green,
   output reg yellow
 );
-
-  //State encoding
+  //2. State Encoding
   localparam RED = 2'b00;
   localparam GREEN = 2'b01;
   localparam YELLOW = 2'b10;
 
+  //3. State Registers (Sequential block)
   reg[1:0] state, next_state;
-
-  //Counter for timing inside state
-  reg[2:0] count; //couting upto 5
-  reg counter_reset;
-
-  //Reset logic
   always @(posedge clk) begin
     if(reset)
       state <= RED;
     else
       state <= next_state;
   end
-  //Counter logic
+  //4. Count Block
+  reg[2:0] count;
+  reg counter_reset;
   always @(posedge clk) begin
     if(reset || counter_reset)
       count <= 0;
     else
       count <= count+1;
   end
-  //Next state logic
+  //5. Next state logic (Combinational block)
   always @(*) begin
     next_state = state;
     counter_reset = 0;
@@ -43,29 +40,23 @@ module traffic_signal_fsm(
           next_state = GREEN;
           counter_reset = 1;
         end
-        else
-          next_state = RED;
       end
       GREEN: begin
         if(count == 4) begin
           next_state = YELLOW;
           counter_reset = 1;
         end
-        else
-          next_state = GREEN;
       end
       YELLOW: begin
         if(count == 1) begin
           next_state = RED;
           counter_reset = 1;
         end
-        else
-          next_state = YELLOW;
       end
       default: next_state = RED;
     endcase
   end
-  //Output logic
+  //6. Output logic
   always @(*) begin
     red = (state == RED);
     green = (state == GREEN);

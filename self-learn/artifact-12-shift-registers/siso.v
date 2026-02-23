6e033a5ce8d17025599b9ec0d161a135b7a9ce4f
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-02-23 20:25:19 +0530

    Create siso.v

---
+++ self-learn/artifact-12-shift-registers/siso.v
@@ -0,0 +1,17 @@
+//RTL Code that implements SISO (Serial In Serial Out) Shift register
+module siso #(parameter WIDTH = 4)(
+  input clk,
+  input reset,
+  input din,
+  output reg dout
+);
+  reg [(WIDTH-1):0] shift_reg;
+  int i;
+  always @(posedge clk) begin
+    if(reset)
+      shift_reg <= 0;
+    else 
+      shift_reg <= {shift_reg[(WIDTH-2):1],din}; //width-1 is the MSB and we don't require LSB, so width-1-1 and concatenate din at lsb.
+  end
+  assign dout = shift_reg[WIDTH-1]; //assigning the LSB to dout
+endmodule

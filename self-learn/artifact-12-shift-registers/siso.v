6f9dbe8ddde87faa919abb64591f7bb184d39a60
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-02-23 20:49:30 +0530

    Update siso.v

--- self-learn/artifact-12-shift-registers/siso.v
+++ self-learn/artifact-12-shift-registers/siso.v
@@ -3,15 +3,14 @@ module siso #(parameter WIDTH = 4)(
   input clk,
   input reset,
   input din,
-  output reg dout
+  output dout
 );
   reg [(WIDTH-1):0] shift_reg;
-  int i;
   always @(posedge clk) begin
     if(reset)
       shift_reg <= 0;
     else 
-      shift_reg <= {shift_reg[(WIDTH-2):0],din}; //width-1 is the MSB and we don't require LSB, so width-1-1 and concatenate din at LSB. [MSB..(LSB-1),din]
+      shift_reg <= {shift_reg[(WIDTH-2):0],din}; //[MSB..(LSB-1),din]
   end
   assign dout = shift_reg[WIDTH-1]; //assigning the MSB to dout
 endmodule

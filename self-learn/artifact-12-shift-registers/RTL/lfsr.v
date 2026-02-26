7d1f21deefe03ce42973f9f2293881e719358521
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-02-26 18:17:43 +0530

    Update lfsr.v

--- self-learn/artifact-12-shift-registers/RTL/lfsr.v
+++ self-learn/artifact-12-shift-registers/RTL/lfsr.v
@@ -5,19 +5,16 @@ module lfsr #(parameter WIDTH = 4)(
   input enable,
   input load,
   input [(WIDTH-1):0] din,
-  output reg [(WIDTH-1):0] dout
+  output [(WIDTH-1):0] dout
 );
   reg [(WIDTH-1):0] shift_reg;
   always @(posedge clk) begin
     if(reset)
-      shift_reg <= 0 + 1'b1 ; //{(WIDTH-1){1'b0},1'b1}
-    else if(din == {WIDTH{1'b0}} 
-      shift_reg <= shift_reg + 1'b1;    //so 0000 case won't be possible
-    else if(load)
+      shift_reg <= {{(WIDTH-1){1'b0}},{1'b1}};
+    else if (load)
       shift_reg <= din;
-    else if(enable)
-      dout <= (din[WIDTH-1] ^ din[WIDTH-2]);
-    else
-      dout <= dout;
+    else if (enable)
+      shift_reg <= {(shift_reg[WIDTH-1]^shift_reg[WIDTH-2]),shift_reg[(WIDTH-1):1]};
   end
+  assign dout = shift_reg;
 endmodule

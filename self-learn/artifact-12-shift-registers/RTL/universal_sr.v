8323dccd6f67d437df6938fc9d332e1786ee5e68
Madhan M.P <129312672+mpmadhan@users.noreply.github.com>
2026-02-27 15:26:55 +0530

    Update universal_sr.v

--- self-learn/artifact-12-shift-registers/RTL/universal_sr.v
+++ self-learn/artifact-12-shift-registers/RTL/universal_sr.v
@@ -1,6 +1,6 @@
 //RTL Code that implements Universal Shift Register
 //Universal shift register, 4 operations: 1.Hold, 2.Shift Right, 3.Shift Left, 4.Parallel load
-module usr #(parameter WIDTH = 4)(
+module universal_sr #(parameter WIDTH = 4)(
   input clk,
   input reset,
   input [1:0] sel,
@@ -10,10 +10,10 @@ module usr #(parameter WIDTH = 4)(
   output [(WIDTH-1):0] dout
 );
   reg [(WIDTH-1):0] shift_reg;
-  always @(posedge clk) begin
+  always @(posedge clk, posedge reset) begin //Asynchronous Behaviour
     if(reset)
       shift_reg <= 0;
-    else begin
+    else if(enable) begin
       case(sel)
         2'b01: shift_reg <= {right_in,shift_reg[(WIDTH-1):1]};
         2'b10: shift_reg <= {shift_reg[(WIDTH-2):0],left_in};
